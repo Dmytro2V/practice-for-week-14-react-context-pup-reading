4b7ada26a697d1af899f708c5886476e6aed38e9
Dmytro2V <op2vin@gmail.com>
2023-03-30 22:17:24 +0300

    working

--- src/components/PupImage/PupImage.js
+++ src/components/PupImage/PupImage.js
@@ -1,8 +1,12 @@
+import {useContext} from 'react';
+import {PupContext} from '.context/PupContext'
+
 import speedy from '../../pups/speedy-pup.jpg';
 import banana from '../../pups/banana-pup.jpg';
 import sleepy from '../../pups/sleepy-pup.jpg';
 
 const PupImage = () => {
+  useContext(PupConttext)
   return (
     <img src={speedy} alt="pup" />
   );
